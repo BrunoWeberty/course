018b0dd7ef8d4e4cac4f8096620d80e229c7d38f
BrunoWeberty <brunoweberty@hotmail.com>
2023-04-26 20:19:11 -0300

    criação da comunicação dos módulos de course e authuser através de uma api utiilizando o Composition Pattern

---
+++ src/main/java/com/ead/course/dtos/UserDto.java
@@ -0,0 +1,19 @@
+package com.ead.course.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    private UUID userId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+}
