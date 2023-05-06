9df7032e4a673278f36f1e58faa1b8dd1f9fd43d
BrunoWeberty <brunoweberty@hotmail.com>
2023-05-06 11:50:17 -0300

    adição de comunicação de deleção no módulo authUser quando realizada deleção de curso é necessário apagar o vinculo de usuário e curso no módulo de authUser

--- src/main/java/com/ead/course/clients/AuthUserClient.java
+++ src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -65,4 +65,9 @@ public class AuthUserClient {
         courseUserDto.setCourseId(courseId);
         restTemplate.postForObject(url, courseUserDto, String.class);
     }
+
+    public void deleteCourseInAuthUser(UUID courseId) {
+        String url = REQUEST_URL_AUTH_USER + "/users/courses/" + courseId;
+        restTemplate.exchange(url, HttpMethod.DELETE, null, String.class);
+    }
 }
